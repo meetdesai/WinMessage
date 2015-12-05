d9150b43179abdd9f3da1385af5418ab80528c78
0xFireball <0xFireball@users.noreply.github.com>
2015-12-05 14:16:57 -0800

    create project

---
+++ WinMessage/WinMessage/MainForm.cs
@@ -0,0 +1,28 @@
+﻿/*
+
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinMessage
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
